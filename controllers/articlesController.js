66b0088ca8d255afb2a725cb7287ce93ec11b04a
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-11-01 00:36:33 -0400

    Added db controller for articles

---
+++ controllers/articlesController.js
@@ -0,0 +1,30 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: function(req, res){ 
+    db.Article 
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(articles => res.json(articles))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res){
+    db.Article
+      .find({ _id: req.params.id })
+      .then(article => res.json(article))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res){ 
+    db.Article
+      .create(req.body)
+      .then(article => res.json(article))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res){
+    db.Article
+      .find({ _id: req.params.id })
+      .then(article => article.remove())
+      .then(removedArticle => res.json(removedArticle))
+      .catch(err => res.status(422).json(err));
+  }
+}
