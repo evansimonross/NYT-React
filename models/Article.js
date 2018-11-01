6841b13b5c2c0a7945209ea1d8ed0768a8776ae1
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-11-01 00:43:41 -0400

    Fixed model

--- models/Article.js
+++ models/Article.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
-  headline: { type: string, required: true },
-  author: { type: string },
-  snippet: { type: string },
-  url: { type: string, required: true }
+  headline: { type: String, required: true },
+  author: { type: String },
+  snippet: { type: String },
+  url: { type: String, required: true }
 });
 
 const Article = mongoose.model("Article", articleSchema);
