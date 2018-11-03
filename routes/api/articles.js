cc70ae0141da8a0369700eb22884390d6447c718
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-11-03 12:02:04 -0400

    Fixed nyt routes

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,36 +1,40 @@
 const router = require("express").Router();
 const articlesController = require("../../controllers/articlesController");
 const axios = require("axios");
+require('dotenv').config()
 
 //    ./api/articles
 router.route("/")
   .get(articlesController.findAll)
   .post(articlesController.create);
 
+//    ./api/articles/search
+router
+  .route("/search")
+  .get(function(req, res){
+    let params = {
+      "api-key": process.env.API_KEY,
+      "q": req.body.q
+    };
+    if(req.body.begin_date) { params.begin_date = req.body.begin_date; }
+    if(req.body.end_date) { params.end_date = req.body.end_date; }
+    axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
+      params: params
+    })
+    .then(function(response){
+      //console.log(response.data.response.docs);
+      res.json(response.data.response.docs);
+    })
+    .catch(function(error){
+      //console.log(error)
+      res.send("ERROR");
+    })
+  });
+
 //    ./api/articles/:id
 router
   .route("/:id")
   .get(articlesController.findById)
   .delete(articlesController.remove);
-
-//    ./api/articles/search
-router
-  .route("/search")
-  .get(function(req, res){
-    axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
-      params: {
-        "api-key": process.env.API_KEY,
-        "q": req.q,
-        "begin_date": req.begin_date,
-        "end_date": req.end_date
-      }
-    })
-    .then(function(data){
-      res.json(data);
-    })
-    .catch(function(err){
-      res.json(err);
-    })
-  });
-
+  
 module.exports = router;
